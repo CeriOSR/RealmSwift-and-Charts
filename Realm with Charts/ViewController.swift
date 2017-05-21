27dc7eaa2c9a816bebae6c0d7c121477432f5863
Rey Cerio <reycerio@Reys-MacBook-Pro.local>
2017-05-20 19:05:54 -0700

    Initial Commit

---
+++ Realm with Charts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Realm with Charts
+//
+//  Created by Rey Cerio on 2017-05-20.
+//  Copyright © 2017 Rey Cerio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
